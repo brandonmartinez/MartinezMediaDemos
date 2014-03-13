603728fca6ad83e3dae6947e9408a7adca610828
Brandon Martinez <me@brandonmartinez.com>
2014-03-13 00:21:08 -0400

    Switched to sample commands

---
+++ MartinezMediaDemos/Controllers/SampleCommandsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using FizzWare.NBuilder;
+using MartinezMediaDemos.Models;
+
+namespace MartinezMediaDemos.Controllers
+{
+    public class SampleCommandsController : ApiController
+    {
+        #region public
+
+        public void Delete(int id) { }
+
+        public IEnumerable<SampleCommand> Get()
+        {
+            var randomNumberGenerator = new Random((int) DateTime.Now.Ticks);
+            var numberOfSampleCommands = randomNumberGenerator.Next(1, 1000);
+
+            return Builder<SampleCommand>.CreateListOfSize(numberOfSampleCommands).All().With(s =>
+            {
+                s.Id = Guid.NewGuid();
+                s.GeneratedAt = DateTime.Now;
+                s.ProcessedAt = null;
+                s.SentAt = null;
+                return s;
+            }).Build();
+        }
+
+        #endregion
+    }
+}
---
+++ MartinezMediaDemos/Models/SampleCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MartinezMediaDemos.Models
+{
+    public class SampleCommand
+    {
+        #region Properties
+
+        public uint Command { get; set; }
+
+        public DateTime GeneratedAt { get; set; }
+
+        public Guid Id { get; set; }
+
+        public DateTime? ProcessedAt { get; set; }
+
+        public DateTime? SentAt { get; set; }
+
+        #endregion
+    }
+}
