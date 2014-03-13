7e45919e3237ff620170417582b56558187b3f3f
Brandon Martinez <me@brandonmartinez.com>
2014-03-13 00:33:04 -0400

    Forgot to add "Data"

--- MartinezMediaDemos/Models/SampleCommand.cs
+++ MartinezMediaDemos/Models/SampleCommand.cs
@@ -8,6 +8,8 @@ namespace MartinezMediaDemos.Models
 
         public uint Command { get; set; }
 
+        public string Data { get; set; }
+
         public DateTime GeneratedAt { get; set; }
 
         public Guid Id { get; set; }
